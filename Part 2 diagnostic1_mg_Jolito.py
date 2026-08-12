7272bc423799d384e6b87fe76110ef2d832bf37b
Jolitosg <jolitosg30@wvc.pshs.edu.ph>
2026-08-12 09:31:03 +0800

    Create Part 2 diagnostic1_mg_Jolito.py

---
+++ Part 2 diagnostic1_mg_Jolito.py
@@ -0,0 +1,5 @@
+bace_price= 10
+pepparoni=int(input("do you want pepparoni topping?: "))
+mushrooms=int(input("do you want mushroom topping?: "))
+extra_cheese=int(input("How much extra mushrooms do you want?: "))
+def  calculate_total(topping_count):
